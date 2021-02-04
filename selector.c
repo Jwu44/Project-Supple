57735b4a1bd0a6e985f79c8b369c9184e52e3d3a
Jwu <justin.wu4444@gmail.com>
2021-02-04 20:03:00 +1100

    first

---
+++ selector.c
@@ -0,0 +1,13 @@
+// selector.c ... implements [insert adt]
+// Give a description of what the file does
+
+#include <assert.h>
+#include <err.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
+#include <math.h>
+// include a .h file
